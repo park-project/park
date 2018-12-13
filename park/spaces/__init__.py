82f7c213484bb1787a8466cb09ee9f18e927e704
Hongzi Mao <hongzi@mit.edu>
2018-12-12 20:49:19 -0500

    add in graph and variable discrete space

--- park/spaces/__init__.py
+++ park/spaces/__init__.py
@@ -1,4 +1,6 @@
 from park.spaces.box import Box
 from park.spaces.discrete import Discrete
+from park.spaces.graph import Graph
 from park.spaces.multi_discrete import MultiDiscrete
 from park.spaces.tuple_space import Tuple
+from park.spaces.variable_discrete import VariableDiscrete
---
+++ park/spaces/graph.py
@@ -0,0 +1,35 @@
+import numpy as np
+from park import core
+from park.spaces.rng import np_random
+
+
+class Graph(core.Space):
+    """
+    The element of this space is a tuple
+    [n * m tensor, n * n sparse matrix]
+    m is number of features associated with each node of the graph(s),
+    n number of nodes, which can be a variable number at each MDP step.
+    """
+    def __init__(self, num_features):
+        self.m = num_features
+        self.n = None
+        core.Space.__init__(self, 'graph_float32', (), np.float32)
+
+    def update(self, n):
+        self.n = n
+
+    def sample(self, valid_list=None):
+        if valid_list is None:
+            return np_random.randint(self.n)
+        else:
+            assert len(valid_list) <= self.n
+            return valid_list[np_random.randint(len(valid_list))]
+
+    def contains(self, x):
+        node_features, adjacency_mat = x
+        return len(node_features.shape) == 2 and \
+               node_features.shape[0] == self.n and \
+               node_features.shape[1] == self.m and \
+               len(adjacency_mat.shape) == 2 and \
+               adjacency_mat.shape[0] == adjacency_mat.shape[1] and \
+               adjacency_mat.shape[0] == self.n
