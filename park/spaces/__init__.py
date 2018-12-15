1c849f9ce03bf8e2bdf96cb5dc198d260dfdeecb
Hongzi Mao <hongzi@mit.edu>
2018-12-15 09:22:49 -0500

    use masked discrete space for graph action space

--- park/spaces/__init__.py
+++ park/spaces/__init__.py
@@ -1,6 +1,7 @@
 from park.spaces.box import Box
 from park.spaces.discrete import Discrete
 from park.spaces.graph import Graph
+from park.spaces.masked_discrete import MaskedDiscrete
 from park.spaces.multi_discrete import MultiDiscrete
 from park.spaces.tuple_space import Tuple
 from park.spaces.variable_discrete import VariableDiscrete
