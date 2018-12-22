fa54bf6433cc6a0c159589593b328541f75feec0
Hongzi Mao <hongzi@mit.edu>
2018-12-22 16:37:13 -0500

    suppress weird unittest warning

--- park/unittest/run_env.py
+++ park/unittest/run_env.py
@@ -1,7 +1,11 @@
+import warnings
 import park
 
 
 def run_env_with_random_agent(env_name, seed):
+    # suppress unittest from throwing weird warnings
+    warnings.simplefilter('ignore', category=ImportWarning)
+
     env = park.make(env_name)
     env.seed(seed)
 
