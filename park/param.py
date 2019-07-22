951a3a79db28a5b578b1d4b1002305648fd78fb0
parimarjan <parimarjannegi@gmail.com>
2019-07-22 06:01:56 +0000

    minor

--- park/param.py
+++ park/param.py
@@ -110,7 +110,7 @@ parser.add_argument('--qopt_max_execution_time', type=int, required=False,
                             default=1200, help='')
 parser.add_argument('--qopt_log_file', type=str, required=False,
                             default="./java.log", help='')
-parser.add_argument('--qopt_scan_cost_factor', type=float, default=0.0,
+parser.add_argument('--qopt_scan_cost_factor', type=float, default=0.2,
                     help="cost of scanning in base table for cost model")
 parser.add_argument('--qopt_use_index_nested_lj', type=int, required=False,
                             default=1,
