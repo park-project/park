ff44fbfa493d740014971848152da2e50188f4a6
parimarjan <parimarjannegi@gmail.com>
2019-10-05 16:19:45 +0000

    pg_cost.py added

---
+++ park/envs/query_optimizer/pg_cost.py
@@ -0,0 +1,26 @@
+
+
+def compute_join_order_loss_pg_single(query, true_cardinalities,
+        est_cardinalities):
+    '''
+    @query: str
+    @true_cardinalities:
+        key:
+            sort([table_1 / alias_1, ..., table_n / alias_n])
+        val:
+            float
+    @est_cardinalities:
+        key:
+            sort([table_1 / alias_1, ..., table_n / alias_n])
+        val:
+            float
+    '''
+
+    # set est cardinalities
+
+
+    # find join order
+
+    # set true cardinalities
+
+    # find cost1, and cost2
